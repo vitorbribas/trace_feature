b97a42d803036b9acbedea59a67661375cb5e1d5
Vítor Ribas <bsbribas66@gmail.com>
2021-08-01 22:28:00 -0300

    style: Pylint updates on gherkin parser and ruby config files

--- trace_feature/core/ruby/ruby_config.py
+++ trace_feature/core/ruby/ruby_config.py
@@ -1,102 +1,110 @@
-from trace_feature.core.base_config import BaseConfig
+"""
+    Module which check and define Ruby configuration
+"""
+
 import os
-import sys
 import re
 import subprocess
 
+from trace_feature.core.base_config import BaseConfig
 
 class RubyConfig(BaseConfig):
+    """
+        Class responsable for checking configuration on a Ruby target project
+    """
+    SIMPLECOV = '  gem \'simplecov-json\'\n'
+    REQSIMCOV = 'require \'simplecov-json\'\n'
+    START = 'SimpleCov.start \'rails\' do\n'
+    EXCLUDE_FOLDERS = '    add_filter [\'migrations\', \'db\', \'.git\', \'features\', ' \
+                      '\'log\', \'public\', \'script\', \'spec\',\'tmp\', \'vendor\', ' \
+                      '\'lib\', \'docker\', \'db\', \'coverage\', \'config\']\nend \n'
+    RESULT_DIR = 'SimpleCov.coverage_dir \'coverage/cucumber\'\n'
+
     def __init__(self):
         pass
 
     def config(self):
-        if self.is_rails_project(self.get_local_path()):
+        project_path = self.__get_local_path()
+        if self.__is_rails_project(project_path):
             print('Rails project!')
-            if self.verify_requirements(self.get_local_path()):
+            if self.__verify_requirements(self, project_path):
                 return True
             else:
-                self.check_gemfile(self.get_local_path())
-                subprocess.call(['bundle', 'install'], cwd=self.get_local_path())
-                self.check_environment(self.get_local_path())
+                self.__check_gemfile(project_path)
+                subprocess.call(['bundle', 'install'], cwd=project_path)
+                return self.__check_environment(project_path)
         else:
             return False
 
-    def is_rails_project(self, path):
-        if os.path.exists(path + "/Gemfile"):
-            return True
-        else:
-            return False
+    @classmethod
+    def __is_rails_project(cls, path):
+        """
+            Check if the target project is a Rails project
+        """
+        return os.path.exists(path + "/Gemfile")
 
-    def get_local_path(self):
+    @classmethod
+    def __get_local_path(cls):
+        """
+            Return the project base path
+        """
         return '.'
 
-    def verify_requirements(self, path):
-        SIMPLECOV = '  gem \'simplecov-json\'\n'
-        REQSIMCOV = 'require \'simplecov-json\''
-        START = 'SimpleCov.start \'rails\' do\n'
-        EXCLUDE_FOLDERS = '    add_filter [\'migrations\', \'db\', \'.git\', \'features\', \'log\', ' \
-                          '\'public\', \'script\', \'spec\', ' \
-                          '\'tmp\', \'vendor\', \'lib\', \'docker\', \'db\', \'coverage\', \'config\']\nend \n'
-        RESULT_DIR = 'SimpleCov.coverage_dir \'coverage/cucumber\''
-
+    @classmethod
+    def __verify_requirements(cls, self, path):
         with open(path+"/Gemfile", 'r') as file:
-            if re.search(re.escape(SIMPLECOV), file.read(), flags=re.M) is None:
+            if re.search(re.escape(self.SIMPLECOV), file.read(), flags=re.M) is None:
                 return False
 
-        file = open(path + "/features/support/env.rb", 'r')
-        text_file = file.read()
-        if re.search(re.escape(REQSIMCOV), text_file, flags=re.M) is None:
-            return False
-        if re.search(re.escape(START), text_file, flags=re.M) is None:
-            return False
-        if re.search(re.escape(RESULT_DIR), text_file, flags=re.M) is None:
-            return False
-        if re.search(re.escape(EXCLUDE_FOLDERS), text_file, flags=re.M) is None:
-            return False
-        return True
+        with open(path + "/features/support/env.rb", 'r') as file:
+            text_file = file.read()
+            if re.search(re.escape(self.REQSIMCOV), text_file, flags=re.M) is None:
+                return False
+            if re.search(re.escape(self.START), text_file, flags=re.M) is None:
+                return False
+            if re.search(re.escape(self.RESULT_DIR), text_file, flags=re.M) is None:
+                return False
+            if re.search(re.escape(self.EXCLUDE_FOLDERS), text_file, flags=re.M) is None:
+                return False
+            return True
 
-    def check_gemfile(self, path):
-        SIMPLECOV = '  gem \'simplecov-json\'\n'
+    def __check_gemfile(self, path):
         output = []
         with open(path + '/Gemfile', 'r+') as file:
             has_simplecov = False
             is_on_test = False
             for line in file:
                 tokens = line.split()
-                if self.is_test_group(tokens):
+                if self.__is_test_group(tokens):
                     is_on_test = True
                 if is_on_test:
                     if not has_simplecov:
-                        has_simplecov = self.simplecov_exists(tokens)
+                        has_simplecov = self.__simplecov_exists(tokens)
                     if tokens:
                         if tokens[0] == 'end':
                             if not has_simplecov:
-                                simplecov_line = SIMPLECOV
+                                simplecov_line = self.SIMPLECOV
                                 output.append(simplecov_line)
                             is_on_test = False
                 output.append(line)
             file.seek(0)
             file.writelines(output)
 
-    def is_test_group(self, tokens):
+    @classmethod
+    def __is_test_group(cls, tokens):
         if len(tokens) > 1:
             if tokens[0] == 'group' and tokens[1] == ':development,' and tokens[2] == ':test':
                 return True
         return False
 
-    def simplecov_exists(self, tokens):
+    @classmethod
+    def __simplecov_exists(cls, tokens):
         if len(tokens) > 1:
             if tokens[0] == 'gem' and tokens[1] == '\'simplecov-json\'':
                 return True
         return False
 
-    def check_environment(self, path):
-        REQSIMCOV = ' require \'simplecov-json\'\n'
-        START = ' SimpleCov.start \'rails\' do\n'
-        EXCLUDE_FOLDERS = '    add_filter [\'migrations\', \'db\', \'.git\', \'features\', \'log\', ' \
-                          '\'public\', \'script\', \'spec\', ' \
-                          '\'tmp\', \'vendor\', \'lib\', \'docker\', \'db\', \'coverage\', \'config\']\nend \n'
-        RESULT_DIR = ' SimpleCov.coverage_dir \'coverage/cucumber\'\n'
+    def __check_environment(self, path):
         output = []
         with open(path + '/features/support/env.rb', 'r+') as file:
             has_req = False
@@ -106,53 +114,55 @@ class RubyConfig(BaseConfig):
 
             for line in file:
                 tokens = line.split()
-                if self.req_simple_cov(tokens):
+                if self.__req_simple_cov(tokens):
                     has_req = True
-                if self.simple_cov_start(tokens):
+                if self.__simple_cov_start(tokens):
                     has_start = True
-                if self.result_dir(tokens):
+                if self.__result_dir(tokens):
                     has_result_dir = True
-                if self.simple_cov_exclude(tokens):
+                if self.__simple_cov_exclude(tokens):
                     has_filter = True
                 output.append(line)
             if not has_req:
-                has_req_line = REQSIMCOV
+                has_req_line = self.REQSIMCOV
                 output.insert(6, has_req_line)
             if not has_result_dir:
-                has_result_line = RESULT_DIR
+                has_result_line = self.RESULT_DIR
                 output.insert(7, has_result_line)
             if not has_start:
-                has_start_line = START
+                has_start_line = self.START
                 output.insert(8, has_start_line)
             if not has_filter:
-                has_exclude_line = EXCLUDE_FOLDERS
+                has_exclude_line = self.EXCLUDE_FOLDERS
                 output.insert(9, has_exclude_line)
 
             file.seek(0)
             file.writelines(output)
 
-    def req_simple_cov(self, tokens):
+    @classmethod
+    def __req_simple_cov(cls, tokens):
         if len(tokens) > 1:
             if tokens[0] == ' require' and tokens[1] == '\'simplecov-json\'':
                 return True
         return False
 
-    def simple_cov_start(self, tokens):
+    @classmethod
+    def __simple_cov_start(cls, tokens):
         if len(tokens) > 1:
             if tokens[0] == ' SimpleCov.start' and tokens[1] == '\'rails\'':
                 return True
         return False
 
-    def simple_cov_exclude(self, tokens):
+    @classmethod
+    def __simple_cov_exclude(cls, tokens):
         if len(tokens) > 1:
             if tokens[0] == ' add_filter':
                 return True
         return False
 
-    def result_dir(self, tokens):
+    @classmethod
+    def __result_dir(cls, tokens):
         if len(tokens) > 1:
             if tokens[0] == ' SimpleCov.coverage_dir and tokens[1] == \'coverage/cucumber\'':
                 return True
         return False
-
-
