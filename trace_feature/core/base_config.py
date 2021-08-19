2b5518b9325fc6c813c8008f0a5a5c33c131d3bf
Helio Adson <helioadson@gmail.com>
2021-08-18 21:17:45 -0300

    style: update base_config.py & base_execution.py according to Pylint check

--- trace_feature/core/base_config.py
+++ trace_feature/core/base_config.py
@@ -1,7 +1,17 @@
+"""
+    Base config for ABC method
+"""
+
 from abc import ABC, abstractmethod
 
 
 class BaseConfig(ABC):
+    """
+        BaseConfig class
+    """
+
     @abstractmethod
     def config(self):
-        pass
+        """
+            BaseConfig config self
+        """
--- trace_feature/core/base_execution.py
+++ trace_feature/core/base_execution.py
@@ -1,19 +1,33 @@
+"""
+    Base execution for ABC method
+"""
+
 from abc import ABC, abstractmethod
 
 
 class BaseExecution(ABC):
+    """
+        BaseExecution class
+    """
+
     @abstractmethod
     def execute(self, path):
-        pass
+        """
+            BaseExecution execute self
+        """
 
     # this method will execute only a specific feature
     @abstractmethod
     def execute_feature(self, path, filename):
-        pass
+        """
+            BaseExecution execute for specific feature
+        """
 
     # this method will execute a specific scenario into a specific feature
     # filename: refer to the .feature file
     # scenario_ref: refer to the line or the name of a specific scenario
     @abstractmethod
     def execute_scenario(self, filename, scenario_ref):
-        pass
+        """
+            BaseExecution execute for specific scenario into a specific feature
+        """
