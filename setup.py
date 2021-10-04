8d74f8de25dc61557be8c77233a4f4b174d9e9a2
Helio Adson <helioadson@gmail.com>
2021-10-03 23:39:17 -0300

    fix: added all trace-feature dependecies

--- setup.py
+++ setup.py
@@ -4,17 +4,19 @@ setup(
 
       name='trace_feature',
       packages=['trace_feature', 'trace_feature.core', 'trace_feature.core.ruby', 'trace_feature.core.features'],
-      version='0.4',
+      version='0.5',
       description='A lib to trace bdd features.',
       url='https://github.com/vitorbribas/trace_feature',
-      download_url = 'https://github.com/vitorbribas/trace_feature/archive/refs/tags/v_04.tar.gz',
+      download_url = 'https://github.com/vitorbribas/trace_feature/archive/refs/tags/v_05.tar.gz',
       author='Rafael Fazzolino',
       author_email='fazzolino29@gmail.com',
       license='MIT',
       keywords = ['BDD', 'Trace'],
       py_modules=['trace_feature'],
       install_requires=[
-            'Click',
+            'Click==7.0',
+            'gherkin-official==4.1.3',
+            'requests==2.21.0'
       ],
       entry_points='''
             [console_scripts]
