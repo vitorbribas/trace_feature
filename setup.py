95a9ee3a9ba08d20c3acec4b14e2c8960d29b257
Helio Adson <helioadson@gmail.com>
2021-10-04 21:22:44 -0300

    feature: added readme to setup.py long description

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-
       name='trace_feature',
       packages=['trace_feature', 'trace_feature.core', 'trace_feature.core.ruby', 'trace_feature.core.features'],
-      version='0.5',
+      version='1.0',
       description='A lib to trace bdd features.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/vitorbribas/trace_feature',
-      download_url = 'https://github.com/vitorbribas/trace_feature/archive/refs/tags/v_05.tar.gz',
+      download_url = 'https://github.com/vitorbribas/trace_feature/archive/refs/tags/v_1.tar.gz',
       author='Rafael Fazzolino',
       author_email='fazzolino29@gmail.com',
       license='MIT',
@@ -16,7 +20,8 @@ setup(
       install_requires=[
             'Click==7.0',
             'gherkin-official==4.1.3',
-            'requests==2.21.0'
+            'requests==2.21.0',
+            'ez-setup==0.9'
       ],
       entry_points='''
             [console_scripts]
